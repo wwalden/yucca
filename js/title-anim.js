1e85e08fbb7f88ca75072ed1f892f9859597f58e
withwalden <74240972+withwalden@users.noreply.github.com>
2022-03-14 10:40:38 +0100

    slider impr with js

--- js/title-anim.js
+++ js/title-anim.js
@@ -44,8 +44,30 @@ const slideAnimNext = () => {
     for (i=1;i<=6;i++) {
       document.querySelector(`.box${i}`).classList.remove('animanext')
     }
-    document.querySelector(`.box0`).classList.remove('animaborn')
-  }, 1200)
+    document.querySelector(`.box0`).classList.remove('animaborn');
     
+    const content0 =  document.querySelector(`.box0`).innerHTML
+    const content1 =  document.querySelector(`.box1`).innerHTML
+    const content2 =  document.querySelector(`.box2`).innerHTML
+    const content3 =  document.querySelector(`.box3`).innerHTML
+    const content4 =  document.querySelector(`.box4`).innerHTML
+    const content5 =  document.querySelector(`.box5`).innerHTML
+    const content6 =  document.querySelector(`.box6`).innerHTML
 
+    document.querySelector(`.box1`).innerHTML = content0
+    document.querySelector(`.box2`).innerHTML = content1
+    document.querySelector(`.box3`).innerHTML = content2
+    document.querySelector(`.box4`).innerHTML = content3
+    document.querySelector(`.box5`).innerHTML = content4
+    document.querySelector(`.box6`).innerHTML = content5
+    document.querySelector(`.box0`).innerHTML = content6
+  
+  }, 1000)
+    
 }
+
+
+
+
+
+
